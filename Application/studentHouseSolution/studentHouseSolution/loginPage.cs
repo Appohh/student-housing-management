cf651a3941012a6e4a7f94fb4de5edcf6d80e162
Kassiya Yuldasheva <franchik10gmail.com>
2023-01-13 15:20:23 +0100

    Ádded feature to save logins. done entirely by me :)

--- Application/studentHouseSolution/studentHouseSolution/loginPage.cs
+++ Application/studentHouseSolution/studentHouseSolution/loginPage.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace studentHouseSolution
 {
@@ -18,17 +19,64 @@ namespace studentHouseSolution
         {
             InitializeComponent();
             persons.getData();
+
+
+            FileStream? fileStream = null;
+
+
+            string email;
+            string password;
+
+            try
+            {
+                fileStream = new FileStream("login.txt", FileMode.OpenOrCreate, FileAccess.Read);
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    if (reader.EndOfStream) return;
+                    email = reader.ReadLine();
+                    if (reader.EndOfStream) return;
+                    password = reader.ReadLine();
+                }
+               
+            }
+            finally
+            {
+                if(fileStream != null)
+                    fileStream.Close();
+            }
+
+            TryLogIn(email, password, false);
+
         }
 
-        private void btnLogin_Click(object sender, EventArgs e)
-        {
-            string email = txtEmail.Text;
-            string password = txtPassword.Text;
 
+        private bool TryLogIn(string email, string password, bool storePassword)
+        {
             foreach (var person in persons.getPersons())
             {
                 if (person.email == email && person.passwordHash == password)
                 {
+                    if (storePassword)
+                    {
+
+                        FileStream? fileStream = null;
+                        try
+                        {
+                            fileStream = new FileStream("login.txt", FileMode.OpenOrCreate, FileAccess.Write);
+
+                            using (StreamWriter writer = new StreamWriter(fileStream))
+                            {
+                                writer.WriteLine(email);
+                                writer.WriteLine(password);
+                            }
+                        }
+                        finally
+                        {
+                            if (fileStream != null)
+                                fileStream.Close();
+                        }
+                    }
+
                     if (person.role == 0)
                     {
                         //MessageBox.Show("Login succes, welcome " + person.firstName + " " + person.lastName + "!");
@@ -36,6 +84,7 @@ namespace studentHouseSolution
                         Dashboard f1 = new Dashboard(person);
                         f1.ShowDialog();
                         this.Close();
+                        return true;
                     }
                     else if (person.role == 1)
                     {
@@ -44,15 +93,26 @@ namespace studentHouseSolution
                         Admin f1 = new Admin(person);
                         f1.ShowDialog();
                         this.Close();
-                    } else
+                        return true;
+                    }
+                    else
                     {
                         MessageBox.Show("Something went wrong, please contact an admin or try again.");
                     }
 
-                } 
+                }
 
             }
             MessageBox.Show("Login not correct");
+            return false;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+            TryLogIn(email, password, true);
+            
         }
     }
 }
